d1388d67e5fd219fe4440a70e59e55a1b6ef83b4
Keegen Knapp <keegenk@gmail.com>
2023-05-06 14:59:02 -0500

    terms of service

---
+++ resources/views/themes/tallstack/terms_service.blade.php
@@ -0,0 +1,24 @@
+@extends('theme::layouts.app')
+
+@section('content')
+
+
+    <div class="py-20 mx-auto max-w-7xl">
+  <div class="py-20 mx-auto max-w-7xl"><h1>Terms of service for PersonalDealer.Site</h1>
+<p>The following terms of service govern the use of PersonalDealer.site. By accessing or using PersonalDealer.site, you agree to be bound by these terms of service.</p>
+<p><strong>General Commercial Terms</strong></p>
+<p>During the term of your order, you will have access to the platform.</p>
+<p>You agree to pay all applicable fees for the Services as set forth on the invoice unless you provide written notice of a dispute regarding such fees no later than 30 days after the invoice date. Any payments you make to us for the Services are final and non-refundable. If we agree to accept your payment via invoice rather than by credit card, full payment must be received within thirty (30) days from the invoice date. We will provide you with notice of non-payment of any undisputed amount due. Unless the full amount not in dispute has been paid, we may suspend your access to the Services thirty (30) days after such notice. We will not suspend access to the Services while you are disputing the applicable charges reasonably and in good faith and are cooperating diligently to resolve the dispute. If your Services are suspended for non-payment, we may charge a re-activation fee to reinstate your access to the Services.</p>
+<p><strong>The subscription term and termination</strong></p>
+<p>Term and Renewal. Your initial Subscription Term will be outlined in the Order Form or Activation Link. At the expiration of the initial period, the Subscription Term will automatically renew for additional periods of the same duration unless either party provides the other party with written notice of non-renewal at least thirty (30) days before the expiration of the then-current term.</p>
+<p>Termination. If either party materially breaches any of its duties or obligations under these Terms of Service or an Order Form, and such breach is not cured within thirty (30) calendar days of the non-breaching party providing the breaching party with written notice of the breach, the non-breaching party may terminate these Terms of Service or the applicable Order Form, as applicable.</p>
+<p>Effect of Termination or Expiration. All provisions of these Terms of Service, which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.</p>
+<p>The payment terms for Services provided by the company require that customers pay the applicable fees as outlined in the invoice, with any disputes regarding such fees to be resolved in good faith between the parties.</p>
+</div>
+</body>
+
+</html>
+
+    </div>
+
+@endsection
