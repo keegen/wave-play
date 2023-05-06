3815d965e55889b0d565fbdd78a63b8f15dde426
Keegen Knapp <keegenk@gmail.com>
2023-05-06 16:47:31 -0500

    testing view

--- resources/views/themes/tallstack/terms.blade.php
+++ resources/views/themes/tallstack/terms.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 
-
+<h1>Test</h1>
+<!-- test
 <div class="py-20 mx-auto max-w-7xl">
   <<h1>Terms of service for PersonalDealer.Site</h1>
   <p>The following terms of service govern the use of PersonalDealer.site. By accessing or using PersonalDealer.site, you agree to be bound by these terms of service.</p>
@@ -14,6 +15,6 @@
   <p>Termination. If either party materially breaches any of its duties or obligations under these Terms of Service or an Order Form, and such breach is not cured within thirty (30) calendar days of the non-breaching party providing the breaching party with written notice of the breach, the non-breaching party may terminate these Terms of Service or the applicable Order Form, as applicable.</p>
   <p>Effect of Termination or Expiration. All provisions of these Terms of Service, which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.</p>
   <p>The payment terms for Services provided by the company require that customers pay the applicable fees as outlined in the invoice, with any disputes regarding such fees to be resolved in good faith between the parties.</p>
-</div>
+</div> -->
 
 @endsection
