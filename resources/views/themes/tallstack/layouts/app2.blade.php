5b7fb119e89e8b8b249783a98d0ab9b12f276d5e
Keegen Knapp <keegenk@gmail.com>
2023-05-09 20:31:45 -0500

    updating app2

--- resources/views/themes/tallstack/layouts/app2.blade.php
+++ resources/views/themes/tallstack/layouts/app2.blade.php
@@ -325,9 +325,32 @@
             <main class="-mt-24 pb-8">
                 <div class="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
                     <h1 class="sr-only">Page title</h1>
+                    
                     <!-- Main 3 column grid -->
                     <div class="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
-                        @yield('content')
+                        <!-- Left column -->
+                        <div class="grid grid-cols-1 gap-4 lg:col-span-2">
+                            <section aria-labelledby="section-1-title">
+                                <h2 class="sr-only" id="section-1-title">Section title</h2>
+                                <div class="overflow-hidden rounded-lg bg-white shadow">
+                                    <div class="p-6">
+                                      @yield('content')
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+
+                        <!-- Right column -->
+                        <div class="grid grid-cols-1 gap-4">
+                            <section aria-labelledby="section-2-title">
+                                <h2 class="sr-only" id="section-2-title">Section title</h2>
+                                <div class="overflow-hidden rounded-lg bg-white shadow">
+                                    <div class="p-6">
+                                        this is a column
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
                     </div>
                 </div>
             </main>
