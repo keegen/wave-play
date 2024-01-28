0dc10801f65b08bc3e1309a6bec4e55a7b896d9b
Keegen Knapp <keegenk@gmail.com>
2024-01-27 18:02:25 -0600

    updated app.js in tallstack

--- resources/views/themes/tallstack/assets/js/app.js
+++ resources/views/themes/tallstack/assets/js/app.js
@@ -1,3 +1,136 @@
+import Alpine from 'alpinejs'
+import axios from 'axios';
+
+window.Alpine = Alpine;
+window.axios = axios;
+
+window.url = document.querySelector("meta[name='url']").getAttribute("content");
+window.csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+
+/** Adds some simple class replacers, see the following article to learn more:
+ * https://devdojo.com/tnylea/animating-tailwind-transitions-on-page-load
+ */
+
+document.addEventListener("DOMContentLoaded", function(){
+    var replacers = document.querySelectorAll('[data-replace]');
+    for(var i=0; i<replacers.length; i++){
+        let replaceClasses = JSON.parse(replacers[i].dataset.replace.replace(/'/g, '"'));
+        Object.keys(replaceClasses).forEach(function(key) {
+            replacers[i].classList.remove(key);
+            replacers[i].classList.add(replaceClasses[key]);
+        });
+    }
+});
+
+/********** ALPINE FUNCTIONALITY **********/
+document.addEventListener('alpine:init', () => {
+    Alpine.store('toast', {
+        type: '',
+        message: '',
+        show: false,
+
+        update({ type, message, show }) {
+            this.type = type;
+            this.message = message;
+            this.show = show;
+        },
+
+        close() {
+            this.show = false;
+        }
+    });
+
+    Alpine.store('plan_modal', {
+        open: false,
+        plan_name: 'basic',
+        plan_id: 0,
+
+        switch(plan_id, plan_name) {
+            this.plan_name = plan_name;
+            this.plan_id = plan_id;
+            this.open = true;
+        },
+
+        close() {
+            this.open = false;
+        }
+    });
+
+    Alpine.store('viewApiKey', {
+        open: false,
+        id: '',
+        name: '',
+        key: '',
+
+        actionClicked(id, name, key) {
+            this.open = true;
+            this.id = id;
+            this.name = name;
+            this.key = key;
+        },
+
+    });
+
+    Alpine.store('editApiKey', {
+        open: false,
+        id: '',
+        name: '',
+        key: '',
+
+        actionClicked(id, name, key) {
+            this.open = true;
+            this.id = id;
+            this.name = name;
+            this.key = key;
+        },
+
+    });
+
+    Alpine.store('deleteApiKey', {
+        open: false,
+        id: '',
+        name: '',
+        key: '',
+
+        actionClicked(id, name, key) {
+            this.open = true;
+            this.id = id;
+            this.name = name;
+            this.key = key;
+        },
+
+    });
+
+    Alpine.store('confirmCancel', {
+        open: false,
+
+        openModal() {
+            this.open = true;
+        },
+
+        close() {
+            this.open = false;
+        }
+
+    });
+
+    Alpine.store('uploadModal', {
+        open: false,
+
+        openModal() {
+            this.open = true;
+        },
+
+        close() {
+            this.open = false;
+        }
+
+    });
+
+});
+
+Alpine.start();
+
 window.url = document.querySelector("meta[name='url']").getAttribute("content");
 window.csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 
