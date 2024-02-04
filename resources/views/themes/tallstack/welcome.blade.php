610b4403fc8b1f3560a234ea6571a1de3e8f22b8
Keegen Knapp <keegenk@gmail.com>
2024-02-03 19:48:27 -0600

    update welcome

--- resources/views/themes/tallstack/welcome.blade.php
+++ resources/views/themes/tallstack/welcome.blade.php
@@ -1,4 +1,6 @@
+@extends('theme::layouts.app3')
 
+@section('content')
 <div class="py-20 mx-auto text-center max-w-7xl">
     <div class="bg-gray-900 px-6 py-24 sm:py-32 lg:px-8 relative overflow-hidden">
         <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
